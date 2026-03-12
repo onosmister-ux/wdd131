afecd36b19758b6c03177353980b4c3c49fa00cf
Rainmaker <onosmister@gmail.com>
2026-03-12 12:28:30 +0100

    optimize images

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,12 +1,12 @@
-const menuButton = document.querySelector("#menu");
-const nav = document.querySelector("nav");
-
-menuButton.addEventListener("click", () => {
-nav.classList.toggle("open");
-});
-
 const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent =
-"Last Modified: " + document.lastModified;
+    "Last Modified: " + document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
