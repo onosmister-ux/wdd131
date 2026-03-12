10aedab0d5eb4064c5cd6687132614f41f4164f4
Rainmaker <onosmister@gmail.com>
2026-03-12 12:15:58 +0100

    temple pic

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+nav.classList.toggle("open");
+});
+
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
