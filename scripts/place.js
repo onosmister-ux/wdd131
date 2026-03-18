d015da3100ca023d482857786d31e5d3e5678f21
Rainmaker <onosmister@gmail.com>
2026-03-18 22:15:45 +0100

    total adjustment

--- scripts/place.js
+++ scripts/place.js
@@ -1,18 +1,22 @@
-const temperature = 10
-const windSpeed = 5
+// STATIC VALUES
+const temperature = 10;
+const windSpeed = 5;
 
-function calculateWindChill(temp, speed){
-return 13.12 + (0.6215 * temp) - (11.37 * speed ** 0.16) + (0.3965 * temp * speed ** 0.16)
+// WIND CHILL FUNCTION
+function calculateWindChill(temp, speed) {
+    return 13.12 + (0.6215 * temp) - (11.37 * speed ** 0.16) + (0.3965 * temp * speed ** 0.16);
 }
 
-let windChill = "N/A"
+// CHECK CONDITIONS
+let windChill = "N/A";
 
-if (temperature <= 10 && windSpeed > 4.8){
-windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C"
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
 }
 
-document.getElementById("windChill").textContent = windChill
+// DISPLAY VALUES
+document.getElementById("windChill").textContent = windChill;
 
-document.getElementById("year").textContent = new Date().getFullYear()
+document.getElementById("year").textContent = new Date().getFullYear();
 
-document.getElementById("lastModified").textContent = document.lastModified
+document.getElementById("lastModified").textContent = document.lastModified;
