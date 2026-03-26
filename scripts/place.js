b50d8d377788fd0fcf2a470bfd3c03b866f47bf0
Rainmaker <onosmister@gmail.com>
2026-03-26 14:49:09 +0100

    final correction

--- scripts/place.js
+++ scripts/place.js
@@ -1,23 +1,18 @@
-// STATIC VALUES
 const temperature = 10;
 const windSpeed = 5;
 
-// WIND CHILL FUNCTION
 function calculateWindChill(temp, speed) {
     return 13.12 + (0.6215 * temp) - (11.37 * speed ** 0.16) + (0.3965 * temp * speed ** 0.16);
 }
 
-// CHECK CONDITIONS
 let windChill = "N/A";
 
 if (temperature <= 10 && windSpeed > 4.8) {
     windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
 }
 
-// DISPLAY VALUES
 document.getElementById("windChill").textContent = windChill;
 
 document.getElementById("year").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
-a
