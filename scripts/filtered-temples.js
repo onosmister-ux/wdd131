a90245ae46d0ddd915f9dabecf540cd806759ea6
Rainmaker <onosmister@gmail.com>
2026-03-27 17:36:14 +0100

    adjustment

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,11 +1,11 @@
 const temples = [
-    { name: "Kirtland Temple", location: "Ohio, USA", dedicated: 1836, area: 15000, image: "https://churchofjesuschristtemples.org/assets/img/temples/kirtland-temple.jpg" },
-    { name: "Salt Lake Temple", location: "Utah, USA", dedicated: 1893, area: 253000, image: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple.jpg" },
-    { name: "Accra Ghana Temple", location: "Ghana", dedicated: 2004, area: 17500, image: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple.jpg" },
-    { name: "Aba Nigeria Temple", location: "Nigeria", dedicated: 2005, area: 11500, image: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple.jpg" },
-    { name: "Rome Italy Temple", location: "Italy", dedicated: 2019, area: 41000, image: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple.jpg" },
-    { name: "Paris France Temple", location: "France", dedicated: 2017, area: 44000, image: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple.jpg" },
-    { name: "Durban South Africa Temple", location: "South Africa", dedicated: 2020, area: 19800, image: "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple.jpg" },
+    { name: "Kirtland Temple", location: "Ohio, USA", dedicated: 1836, area: 15000, image: "https://churchofjesuschristtemples.org/assets/img/temples/kirtland-temple/kirtland-temple-1275-main.jpg" },
+    { name: "Salt Lake Temple", location: "Utah, USA", dedicated: 1893, area: 253000, image: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-15669-main.jpg" },
+    { name: "Accra Ghana Temple", location: "Ghana", dedicated: 2004, area: 17500, image: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-13760-main.jpg" },
+    { name: "Aba Nigeria Temple", location: "Nigeria", dedicated: 2005, area: 11500, image: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg" },
+    { name: "Rome Italy Temple", location: "Italy", dedicated: 2019, area: 41000, image: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg" },
+    { name: "Paris France Temple", location: "France", dedicated: 2017, area: 44000, image: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg" },
+    { name: "Durban South Africa Temple", location: "South Africa", dedicated: 2020, area: 19800, image: "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple/durban-south-africa-temple-7936-main.jpg" },
     { name: "Freiberg Germany Temple", location: "Germany", dedicated: 1985, area: 21000, image: "https://churchofjesuschristtemples.org/assets/img/temples/freiberg-germany-temple.jpg" },
     { name: "Manila Philippines Temple", location: "Philippines", dedicated: 1984, area: 26600, image: "https://churchofjesuschristtemples.org/assets/img/temples/manila-philippines-temple.jpg" }
 ];
