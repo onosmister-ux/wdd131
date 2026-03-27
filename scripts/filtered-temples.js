9213a6e0886fdc39e339436df9298372963ffab0
Rainmaker <onosmister@gmail.com>
2026-03-28 00:17:48 +0100

    js adjustment

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -6,8 +6,8 @@ const temples = [
     { name: "Rome Italy Temple", location: "Italy", dedicated: 2019, area: 41000, image: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg" },
     { name: "Paris France Temple", location: "France", dedicated: 2017, area: 44000, image: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg" },
     { name: "Durban South Africa Temple", location: "South Africa", dedicated: 2020, area: 19800, image: "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple/durban-south-africa-temple-7936-main.jpg" },
-    { name: "Freiberg Germany Temple", location: "Germany", dedicated: 1985, area: 21000, image: "https://churchofjesuschristtemples.org/assets/img/temples/freiberg-germany-temple.jpg" },
-    { name: "Manila Philippines Temple", location: "Philippines", dedicated: 1984, area: 26600, image: "https://churchofjesuschristtemples.org/assets/img/temples/manila-philippines-temple.jpg" }
+    { name: "Freiberg Germany Temple", location: "Germany", dedicated: 1985, area: 21000, image: "https://churchofjesuschristtemples.org/assets/img/temples/freiberg-germany-temple/freiberg-germany-temple-16459-main.jpg" },
+    { name: "Manila Philippines Temple", location: "Philippines", dedicated: 1984, area: 26600, image: "https://churchofjesuschristtemples.org/assets/img/temples/_temp/029-Manila-Philippines-Temple.jpg" }
 ];
 
 function displayTemples(list) {
